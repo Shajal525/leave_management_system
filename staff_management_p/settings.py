5a61f638a7e1543177fea5c94e8f547bc860522d
shajal525 <shajal525@gmail.com>
2019-08-24 18:16:01 +0600

    Environment Error fix

--- staff_management_p/settings.py
+++ staff_management_p/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
